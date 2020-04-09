942f14b11f4b3dca51e660ed027bb41c9d3c6b45
sanchit05 <sanchitjohari5@gmail.com>
2020-04-09 12:07:14 +0530

    updated conract with test cases

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,14 +1,25 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 contract Election{
-   
-   string public candidate;
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
 
    constructor() public{
- 
-   candidate= "sanchit";
- 
+      addCandidate("Candidate 1");
+      addCandidate("Candidate 2");
    }
- 
 
+   function addCandidate(string memory _name)private{
+
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+
+    }
 }
